b3bcc998c580cf2bf02a073f3fdb1d5741564cc4
Lina Sachuk <sachuklina@gmail.com>
2020-11-24 23:37:16 -0700

    add 8-inheritance file

---
+++ 8-inheritance.ts
@@ -0,0 +1,40 @@
+// Base/ Parent Class
+export class Animal {
+    age: number;
+    legs: number;
+    name: string;
+
+    constructor(age: number, legs:number, name:string){
+        this.age = age;
+        this.legs = age;
+        this.name = name;
+    }
+
+}
+
+// Derived  / Child Class(s)
+
+export class Dog extends Animal {
+
+    woof(): string {
+        return 'Woof! Woof!';
+    }
+
+}
+
+const dog = new Dog(2,4, 'Gator');
+dog.name;
+dog.woof();
+
+
+export class Cat extends Animal {
+
+    meow(): string {
+        return 'Meow! Meow!';
+    }
+
+}
+
+const cat = new Cat(19, 5, 'Baby');
+cat.age;
+cat.meow();
