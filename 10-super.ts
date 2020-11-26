709d8f428cee80cfcdff810cd149df545f188d39
Lina Sachuk <sachuklina@gmail.com>
2020-11-25 23:27:23 -0700

    add super 10-super file

---
+++ 10-super.ts
@@ -0,0 +1,33 @@
+import { Dog } from "./8-inheritance";
+
+export class Animal {
+    age: number;
+    legs: number;
+    name: string;
+    
+    constructor(age: number, legs: number, name: string) {
+        this.age = age;
+        this.legs = legs;
+        this.name = name;
+    }
+
+}
+
+export class Cat extends Animal {
+    constructor(data: { age: number, legs: number, name: string }) {
+        super(data.age, data.legs, data.name);
+    }
+
+    meow(): string {
+        return 'MEOW! HISS! HISS!';
+    }
+}
+
+
+const dog = new Dog();
+const cat = new Cat({age:19, legs:5, name:'Baby'});
+console.log(dog instanceof Animal);
+// false
+console.log(cat instanceof Animal);
+// true
+
