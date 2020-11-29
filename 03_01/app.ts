e338c50772831b774181da22acc3889b84870df0
Lina Sachuk <sachuklina@gmail.com>
2020-11-29 14:16:31 -0700

    add 3rd part files

---
+++ 03_01/app.ts
@@ -0,0 +1,10 @@
+let warriorName = 'Manny';
+
+const warriorType = 'Samurai';
+
+warriorName = 'Emmanuel';
+
+// Cannot assign to 'warriorType' because it is a constant.
+warriorType = 'Archer';
+
+console.log(warriorName, warriorType);
